79829be8fe854fcdbb3c1b37fe6a35652c53a06c
hdinjos <hdinjos@gmail.com>
2025-12-28 17:21:03 +0700

    feat: first init

---
+++ app/Traits/HasSnowflakeId.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use Godruoyi\Snowflake\Snowflake;
+
+trait HasSnowflakeId
+{
+    /**
+     * Boot trait untuk auto generate snowflake id
+     */
+    protected static function bootHasSnowflakeId() {
+        static::creating(function($model){
+            if(!$model->getKey()){
+                $model->{$model->getKeyName()} = self::generateSnowflakeId();
+            }
+        });
+    }
+
+    /**
+     * Generate Snowflake ID
+     */
+    protected static function generateSnowflakeId(): int {
+        static $snowflake;
+
+        if(!$snowflake){
+            $snowflake = new Snowflake(
+                config('snowflakeid.datacenter_id'),
+                config('snowflakeid.worker_id')
+            );
+        }
+
+        return $snowflake->id();
+    }
+}
---
+++ config/snowflake.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'datacenter_id' => env('SNOWFLAKE_DATACENTER_ID', 1),
+    'worker_id'     => env('SNOWFLAKE_WORKER_ID', 1),
+];
