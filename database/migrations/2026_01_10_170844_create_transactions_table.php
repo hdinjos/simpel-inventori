2525a45ddbd80511377e24328d0d3aaf3048f13e
Ghufron <groyani464@gmail.com>
2026-01-11 00:48:26 +0700

    feat: make migration transaction table

---
+++ database/migrations/2026_01_10_170844_create_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Enums\TransactionType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigInteger('id')->primary();
+            $table->string('identifier')->unique(); //TRX-month-year-number-incrementing
+            $table->bigInteger('user_id');
+            $table->bigInteger('partner_id');
+            $table->enum('type', TransactionType::cases()); // in | out
+            $table->text('note');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->foreign('partner_id')->references('id')->on('partners')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
